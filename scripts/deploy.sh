454f5fead6ddf32c47e24acdfa4b91c31b6223a4
Mohammed Khaja Pasha <mohammedkhwaja25-afk@gmail.com>
2026-07-23 08:01:46 +0000

    Add automatic Kubernetes deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,15 @@
 
 set -e
 
-echo "Updating Kubernetes deployment..."
+echo "=================================="
+echo "Starting Kubernetes Deployment..."
+echo "=================================="
 
 kubectl set image deployment/employee-portal \
-employee-portal=mohammedkhaja25/employee-portal:latest
+employee-portal=${DOCKER_USERNAME}/employee-portal:latest
 
 kubectl rollout status deployment/employee-portal
 
+echo "=================================="
 echo "Deployment completed successfully!"
+echo "=================================="
